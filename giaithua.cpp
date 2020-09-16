c8a6a5351f061ec3bbced6b374849d62b279164d
anislearncode <71391400+anislearncode@users.noreply.github.com>
2020-09-16 07:19:40 -0700

    Add files via upload

---
+++ giaithua.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, tich = 1;
+    cout << "Nhap vao 1 so nguyen duong N: " << endl;
+    cin >> n;
+    for (int i=2; i<=n; i++) {
+        tich *= i;
+    }
+    cout << "Giai thua cua " << n << " : " << tich << endl;
+
+    system("pause");
+    return 0;
+}
