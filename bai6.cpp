0b750475fd204517648b23173f376cf2c487cdac
anislearncode <71391400+anislearncode@users.noreply.github.com>
2020-09-17 04:08:57 -0700

    Fix lỗi bài 6
    Tui vừa fix lỗi bài 6 vì khi tui copy & paste code lên codefun.vn thì nó thông báo lỗi. Tưởng gì chứ hóa ra nó lỗi vì thừa 1 dấu cách :V

--- bai6.cpp
+++ bai6.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     string z;
     cin >> z;
-    for (int i=z.length(); i>=0; i--) cout << z[i] << endl;
+    for (int i=z.length()-1; i>=0; i--) cout << z[i];
 
     system("pause");
     return 0;
