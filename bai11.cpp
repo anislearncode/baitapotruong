ffd10e08b669d82b1c330ae385f6bf829aadb6f9
anislearncode <71391400+anislearncode@users.noreply.github.com>
2020-09-17 05:57:25 -0700

    Bài 11 nhưng tôi code bị lỗi

---
+++ bai11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    double C, S;
+    cin >> a >> b >> c;
+    C = (a + b + c)/2;
+    S = sqrt(C*(C-a)*(C-b)*(C-c));
+    cout << S << endl;
+
+    system("pause");
+    return 0;
+}
