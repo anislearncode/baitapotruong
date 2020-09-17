887cbf7c7376725de79360da0ddcbe64294cde63
anislearncode <71391400+anislearncode@users.noreply.github.com>
2020-09-17 04:11:38 -0700

    Fix lỗi bài 4
    Lỗi thừa 1 số trong mảng

--- bai4.cpp
+++ bai4.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int n, count = 0;
     cin >> n;
-    for (int i=1; i<=n; i++) {
+    for (int i=1; i<n; i++) {
         if (i>((n-1)/2)) count += 1;
     }
     cout << count << endl;
