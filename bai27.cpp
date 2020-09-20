ae4b90148ebad3bcfb0d4fcb1b697fc5caa85620
anislearncode <71391400+anislearncode@users.noreply.github.com>
2020-09-19 18:32:53 -0700

    Thêm chú thích

--- bai27.cpp
+++ bai27.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 
 int main() {
-    int n, UocNhoNhat;
-    bool check = true;
-    cin >> n;
-    if (n>=2) {
-        for ( int i=2; i<=round(sqrt(n)); i++ ) {
-            if (n % i == 0) {
-                check=false;
-                UocNhoNhat = i;
-                break;
+    int n, UocNhoNhat; //B1: Khai báo biến
+    bool check = true; //B2: Đặt biến check=true
+    cin >> n; //B3: Nhập vào số nguyên n;
+    if (n>=2) { //B4: i>=2 ?
+        for ( int i=2; i<=round(sqrt(n)); i++ ) { //B4.1: Nếu i>=2 thì lặp i từ 2 tới căn n
+            if (n % i == 0) { //B4.1.1: Nếu n chia hết cho một số i bất kỳ từ 2 tới căn n
+                check=false; //B4.1.1.1: Thì chuyển biến check=true
+                UocNhoNhat = i; //B4.1.1.2: Đặt ước nhỏ nhất bằng i mà số n chia hết cho i(Nhìn B4.1)
+                break; //B4.1.1.3: Dừng vòng lặp, xuống B5
             }
         }
-    } else check=false;
-    if (check==true) cout << "YES" << endl;
-    else cout << UocNhoNhat << endl;
+    } else check=false; //B4.2: Nếu n<2 thì đặt biến check=false(Nhìn B4 để hiểu rõ hơn)
+    if (check==true) cout << "YES" << endl; // Nếu check=true(có nghĩa là n không chia hết cho số i bất kỳ nào từ 1 tới căn n) thì in ra màn hình "YES"
+    else cout << UocNhoNhat << endl; //Còn nếu check=false thì in ra màn hình "NO"
 
     system("pause");
     return 0;
