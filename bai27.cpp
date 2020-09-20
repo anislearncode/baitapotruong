57156f69c20b374d90068d57c90c23cf8ec61cef
anislearncode <71391400+anislearncode@users.noreply.github.com>
2020-09-19 18:20:26 -0700

    Create bai27.cpp

---
+++ bai27.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    int n, UocNhoNhat;
+    bool check = true;
+    cin >> n;
+    if (n>=2) {
+        for ( int i=2; i<=round(sqrt(n)); i++ ) {
+            if (n % i == 0) {
+                check=false;
+                UocNhoNhat = i;
+                break;
+            }
+        }
+    } else check=false;
+    if (check==true) cout << "YES" << endl;
+    else cout << UocNhoNhat << endl;
+
+    system("pause");
+    return 0;
+}
